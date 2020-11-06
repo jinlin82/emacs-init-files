7d47bca33455871ee50eb2174a2997bc7bc7cbbf
Jin Lin <jinlin82@qq.com>
2020-11-06 13:00:28 +0800

    add switch window

--- edit.el
+++ edit.el
@@ -466,6 +466,24 @@ Version 2017-01-15"
 (global-set-key (kbd "M-g w") 'avy-goto-word-1)
 (global-set-key (kbd "M-g e") 'avy-goto-word-0)
 
+;; -------------------------- switch-window -------------------------
+(require 'switch-window)
+(global-set-key (kbd "C-x o") 'switch-window)
+(global-set-key (kbd "C-x 1") 'switch-window-then-maximize)
+(global-set-key (kbd "C-x 2") 'switch-window-then-split-below)
+(global-set-key (kbd "C-x 3") 'switch-window-then-split-right)
+(global-set-key (kbd "C-x 0") 'switch-window-then-delete)
+
+(global-set-key (kbd "C-x 4 d") 'switch-window-then-dired)
+(global-set-key (kbd "C-x 4 f") 'switch-window-then-find-file)
+(global-set-key (kbd "C-x 4 m") 'switch-window-then-compose-mail)
+(global-set-key (kbd "C-x 4 r") 'switch-window-then-find-file-read-only)
+
+(global-set-key (kbd "C-x 4 C-f") 'switch-window-then-find-file)
+(global-set-key (kbd "C-x 4 C-o") 'switch-window-then-display-buffer)
+
+(global-set-key (kbd "C-x 4 0") 'switch-window-then-kill-buffer)
+
 ;;-----------------------------------Buffer Menu--------------------------------------
 ; eliminate the need for ‘C-x o’, making list-buffers much easier to use
 (global-set-key (kbd "\C-x\C-b") 'buffer-menu-other-window)
